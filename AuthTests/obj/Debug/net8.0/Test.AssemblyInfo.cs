e6585730b2818b28bed726ed2ea6253ecffd43ca
ANGELO GABRIEL WONG GUTIERREZ <awong@edu.upolitecnica.cr>
2024-12-19 18:17:08 -0600

    Ultimos cambios Parte 4 Se corrigio errires de titulos y mejoras a ciertos blazor

--- AuthTests/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ AuthTests/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c49aed8fbac9c5480e2b2faa4e36472e66190be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62dd287c0cc896ce56c2bd981bf7457beb04475f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProyectoGimnacioV2/obj/Debug/net8.0/ProyectoGimnacioV2.AssemblyInfo.cs
+++ ProyectoGimnacioV2/obj/Debug/net8.0/ProyectoGimnacioV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoGimnacioV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c49aed8fbac9c5480e2b2faa4e36472e66190be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62dd287c0cc896ce56c2bd981bf7457beb04475f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoGimnacioV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoGimnacioV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
